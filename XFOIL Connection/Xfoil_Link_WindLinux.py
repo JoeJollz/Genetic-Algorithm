901d3f23b814021001d958eca7742333c2ad9de7
JoeJollz <124887383+JoeJollz@users.noreply.github.com>
2024-04-27 11:58:43 +0100

    Add files via upload

---
+++ XFOIL Connection/Xfoil_Link_WindLinux.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 27 11:35:41 2024
+
+@author: jrjol
+"""
+
+from io import *
+import os
+import re
+
+#open the file for the xfoil commands
+out=open('xfoilinput.txt', 'w')
+
+# Write xfoil commands to the file (as they would 
+# be typed into xfoil)
+out.write(
+"""
+naca 4415
+panel
+OPER
+V 1e-5
+Re 1e5
+A 5
+!
+
+quit
+"""
+)
+
+#Close the file to make sure the file is written 
+#to disk properly
+out.close() 
+
+#call the system command to run xfoil and redirect 
+#our input file into it
+os.system('xfoil < xfoilinput.txt > xfoiloutput.txt')
+
+#Using the python popen command, the output from this can 
+#be redirected from the standard output using the read() 
+#method.
+
+# Read the file and get the last 10 lines
+text = open("xfoiloutput.txt").readlines()[-10:]
+
+# Regular expression patterns
+cl_pattern = r'CL =\s*([0-9.]+)'  # Matches CL value
+cd_pattern = r'CD =\s*([0-9.]+)'  # Matches CD value
+
+# Search for CL value
+matches = re.search(cl_pattern, text[5])
+if matches:
+    cl = matches.group(1)
+    print("Lift coefficient =", cl)
+else:
+    print("CL not found")
+
+# Search for CD value
+matches = re.search(cd_pattern, text[6])
+if matches:
+    cd = matches.group(1)
+    print("Drag coefficient =", cd)
+else:
+    print("CD not found")
